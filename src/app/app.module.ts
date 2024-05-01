25c8f64d0909f19724b58140e13a0cb210815a70
TanmayKhule97 <khuletanmay315@gmail.com>
2024-05-01 15:29:14 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    QuestionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/question/question.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../service/question.service';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+  public name:string | null="";
+  public questionList:any = [];
+  public currentquestion:number=0;
+  public points: number = 0;
+  counter = 60;
+  correctAns:number=0;
+  IncorrectAns:number=0;
+  interval$:any;
+  constructor(private questservice:QuestionService) { }
+
+  ngOnInit(): void {
+    this.name = localStorage.getItem('name');
+    this.getallquestion();
+    this.startcounter();
+  }
+  getallquestion(){
+    this.questservice.getQuestionjson().subscribe(res=>{
+      this.questionList=res.questions
+      console.log(this.questionList[this.currentquestion]?.options)
+    })
+  }
+  nextquestion(){
+    this.currentquestion=this.currentquestion+1
+    console.log(this.currentquestion)
+  }
+  previousquestion(){
+
+
+    this.currentquestion=this.currentquestion-1
+    console.log(this.currentquestion)
+  }
+  answer(currentquestion:number ,option:any){
+        if(option.correct){
+          this.points=this.points+10;
+          this.correctAns=this.correctAns+1;
+          this.currentquestion=this.currentquestion+1
+        }else{
+          this.points=this.points-10;
+          this.IncorrectAns=this.IncorrectAns-1;
+          this.currentquestion=this.currentquestion+1
+        }
+  }
+  startcounter(){
+    this.interval$= interval(1000).subscribe(val=>{
+      this.counter=this.counter-1;
+      if(this.counter===0){
+        this.currentquestion=this.currentquestion+1;
+        this.points=this.points-10;
+        this.counter=60;
+      }
+    })
+    setTimeout(()=>{
+      this.interval$.unsubscribe()
+    },600000)
+  }
+  stopcounter(){
+    this.interval$.unsubscribe();
+    this.counter=0;
+  }
+  resettcounter(){
+    this.stopcounter();
+    this.counter=60;
+    this.startcounter();
+  }
+  resetquiz(){
+    this.resettcounter();
+    this.getallquestion();
+    this.counter=60;
+    this.currentquestion=0
+  }
+}
